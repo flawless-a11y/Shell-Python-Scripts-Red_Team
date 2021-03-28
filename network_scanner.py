071cc9bfcdbff2a9223e683c7bea3b3bbfebf4d3
hawx-23 <71605981+hawx-23@users.noreply.github.com>
2021-03-28 12:22:58 +0530

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -7,9 +7,11 @@ def scan(ip):
     broadcast =scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast =broadcast/arp_request
     answered_list =scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
-    print("IP\t\t\tMAC Address \n---------------------------------------------------")
+    print("IP\t\t\t\tMAC Address \n---------------------------------------------------")
     for element in answered_list:
-        print(element[1].psrc+"\t\t"+element[1].hwsrc)
+        print(element[1].psrc+"\t\t\t"+element[1].hwsrc)
 
 
-scan("192.168.0.1/24")
+print("Please specify the subnet range to scan (example : 10.0.0.1/24 )")
+ip=str(input())
+scan(ip)
