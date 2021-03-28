6d6b8174b4d6bb29d9256a3ab50f44e13c97bc90
hawx-23 <71605981+hawx-23@users.noreply.github.com>
2021-03-28 13:22:22 +0530

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,13 @@
 #! /usr/bin/env python
 
+import optparse
 import scapy.all as scapy
+
+def get_arguments():
+    parser=optparse.OptionParser()
+    parser.add_option("-t","--target",dest="target", help="Target IP/IP range ")
+    options=parser.parse_args()[0]
+    return options
 
 def scan(ip):
     arp_request =scapy.ARP(pdst=ip)
@@ -8,10 +15,14 @@ def scan(ip):
     arp_request_broadcast =broadcast/arp_request
     answered_list =scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
     print("IP\t\t\t\tMAC Address \n---------------------------------------------------")
+    response_list=[]
     for element in answered_list:
-        print(element[1].psrc+"\t\t\t"+element[1].hwsrc)
+        sub_entries={"ip":element[1].psrc,"mac":element[1].hwsrc}
+        response_list.append(sub_entries)
+    return response_list
+options=get_arguments()
+scan_results=scan(options.target)
+print("IP\t\t\tMAC Address\n-----------------------------------------------------------------------------")
+for entries in scan_results:
+    print(entries["ip"]+"\t\t"+entries["mac"])
 
-
-print("Please specify the subnet range to scan (example : 10.0.0.1/24 )")
-ip=str(input())
-scan(ip)
